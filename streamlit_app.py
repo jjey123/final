aa31147d6241a7f4203eef2f7ccda06aee49c72e
jjey123 <airodjhon25@gmail.com>
2024-12-13 00:25:21 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ st.write(
 @st.cache_data
 def load_data():
     """Load NBA dataset."""
-    return pd.read_csv("/content/nba_data_processed.csv")
+    return pd.read_csv("nba_data_processed.csv")
 
 # Load the dataset
 df = load_data()
